9f9e739fd7073826c2ec15bf349ebbf0af5a285e
Angela Chen <jiewenc@andrew.cmu.edu>
2020-09-16 13:32:44 +0000

    Subscriber custom login (without accessing dashboard at all); temporary css for custom login page

--- wp-content/themes/museum-live-theme/header.php
+++ wp-content/themes/museum-live-theme/header.php
@@ -24,8 +24,15 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo esc_url(site_url("/wp-signup.php")); ?> ">Signup <i class="fas fa-user-plus"></i></a></li>
-        <li><a href="<?php echo esc_url(site_url("/wp-login.php")); ?>">Login <i class="fas fa-user"></i></a></li>
+      <?php if(is_user_logged_in()) { ?>
+        <li><a class="btn-with-image" href="<?php echo wp_logout_url(); ?> ">
+        <span id="site_header__avatar"><?php echo get_avatar(get_current_user_id(), 20);  ?></span>
+        <span>Logout <i class="fas fa-user-plus"></i></a></li></span>
+        <?php } else { ?>
+            <li><a href="<?php echo wp_registration_url(); ?> ">Signup <i class="fas fa-user-plus"></i></a></li>
+            <li><a href="<?php echo wp_login_url(); ?>">Login <i class="fas fa-user"></i></a></li>
+        <?php } ?>
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
