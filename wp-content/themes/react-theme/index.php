0bd44df6682c3c354d7499a71fc676cc1e7be0f3
Angela Chen <jiewenc@andrew.cmu.edu>
2020-11-23 19:52:41 +0000

    banner css fix (based on Kat's note)

--- wp-content/themes/react-theme/index.php
+++ wp-content/themes/react-theme/index.php
@@ -46,7 +46,7 @@
         Learn how to configure a non-root public URL by running `npm run wpbuild`.
     -->
     <title>Museum.Live</title>
-<link href="/wp-content/themes/react-theme/static/css/1.chunk.css?143a3336d67ec35d7346" rel="stylesheet"><link href="/wp-content/themes/react-theme/static/css/main.chunk.css?143a3336d67ec35d7346" rel="stylesheet"></head>
+<link href="/wp-content/themes/react-theme/static/css/0.chunk.css?78dfb53a4699d5432a5e" rel="stylesheet"><link href="/wp-content/themes/react-theme/static/css/main.chunk.css?78dfb53a4699d5432a5e" rel="stylesheet"></head>
     <body>
     <noscript>
         You need to enable JavaScript to run this app.
@@ -103,5 +103,5 @@
 
     </script>
     <script src="https://kit.fontawesome.com/91b7b285ec.js" crossorigin="anonymous"></script>
-    <script src="/wp-content/themes/react-theme/static/js/bundle.js?143a3336d67ec35d7346"></script><script src="/wp-content/themes/react-theme/static/js/1.chunk.js?143a3336d67ec35d7346"></script><script src="/wp-content/themes/react-theme/static/js/main.chunk.js?143a3336d67ec35d7346"></script></body>
+    <script src="/wp-content/themes/react-theme/static/js/bundle.js?78dfb53a4699d5432a5e"></script><script src="/wp-content/themes/react-theme/static/js/0.chunk.js?78dfb53a4699d5432a5e"></script><script src="/wp-content/themes/react-theme/static/js/main.chunk.js?78dfb53a4699d5432a5e"></script></body>
 </html>
